1e967dbb1554817223d0a5bd6b4c1cb7aa83bb56
Eren Akdeniz <erenakdeniz0223@gmail.com>
2023-11-04 23:43:57 +0100

    rename

--- authorize.js
+++ authorize.js
@@ -1,6 +1,6 @@
 function authorizeSpotify() {
     const clientId = 'e700f259fe5e46018887d33fe46c93be';
-    const redirectUri = 'https://eren-a.github.io/spotify-stats/overview.html';
+    const redirectUri = 'https://eren-a.github.io/spotify-stats/authorized.html';
     const scopes = 'user-top-read'; // Add necessary scopes
 
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}&response_type=token`;
