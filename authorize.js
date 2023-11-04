e6412fd1deab37267588728337b3a29cb1898a99
Eren Akdeniz <erenakdeniz0223@gmail.com>
2023-11-04 19:47:13 +0100

    fix redirecturi

--- authorize.js
+++ authorize.js
@@ -1,6 +1,6 @@
 function authorizeSpotify() {
     const clientId = 'e700f259fe5e46018887d33fe46c93be';
-    const redirectUri = 'http://127.0.0.1:5500/anbindung/overview.html';
+    const redirectUri = 'https://eren-a.github.io/spotify-stats/overview.html';
     const scopes = 'user-top-read'; // Add necessary scopes
 
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}&response_type=token`;
