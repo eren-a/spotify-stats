7057a7120a7d69d505579715f588904bbc1e5755
Eren Akdeniz <erenakdeniz0223@gmail.com>
2024-02-29 12:41:22 +0100

    updated README and removed unused variable

--- README.md
+++ README.md
@@ -1,4 +1,5 @@
 # Check your own Spotify stats!
 
+(Does not work, I have to manually add the users e-mail to my spotify api dashboard to give you access)\
 
 Discover your Spotify stats with just a click. Visit this [link](https://eren-a.github.io/spotify-stats/) to explore your favorite artists and top songs in different time intervalls.
--- authorize.js
+++ authorize.js
@@ -1,7 +1,7 @@
 function authorizeSpotify() {
     const clientId = 'e700f259fe5e46018887d33fe46c93be';
     const redirectUri = 'https://eren-a.github.io/spotify-stats/authorized.html';
-    const scopes = 'user-top-read'; // Add necessary scopes
+    const scopes = 'user-top-read';
 
     const authUrl = `https://accounts.spotify.com/authorize?client_id=${clientId}&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${encodeURIComponent(scopes)}&response_type=token`;
 
